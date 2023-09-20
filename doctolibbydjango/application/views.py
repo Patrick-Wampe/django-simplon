378cccdbd4cc783372598fc39ebd7b14bb13002f
Patrick-Wampe <47600274+Patrick-Wampe@users.noreply.github.com>
2023-09-20 15:50:03 +0200

    Update views.py

--- doctolibbydjango/application/views.py
+++ doctolibbydjango/application/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+from authentification.models import Utilisateur
 
 @login_required
 def accueil(request):
-    prenom = "Patou"
+    prenom = request.user.username
     return render(request,"accueil.html",
                   context={"prenom": prenom})
+
+
+@login_required
+def comptes(request):
+    regexMDP = "^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&*()_+-]).{8,}$"
+    message = ""
+    if request.method == "POST":
+        ancienMDP = request.POST["ancienMDP"]
+        nouveauMDP1 = request.POST["nouveauMDP1"]
+        nouveauMDP2 = request.POST["nouveauMDP2"]
+        
+        verification = authenticate(username = request.user.username,
+                                    password = ancienMDP)
+        if verification != None:
+            if nouveauMDP1 == nouveauMDP2:
+                utilisateur = Utilisateur.objects.get(username = request.user.username)
+                #utilisateur = Utilisateur.objects.get(id=request.user.id)
+                utilisateur.set_password(request.POST.get("nouveauMDP1"))
+                utilisateur.save()
+                return redirect("accueil")
+            else:
+                message = "⚠️ Les deux mot de passe ne concordent pas ⚠️"
+        else:
+            message = "L'ancien mot de passe n'est pas bon. T'es qui toi ? 😡"
+    return render(request,
+                  "comptes.html",
+                  {"regexMDP" : regexMDP, "message" : message})
+
