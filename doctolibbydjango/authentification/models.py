2908af797f71eec850e6949a3800ccf267b8acd8
Patrick-Wampe <47600274+Patrick-Wampe@users.noreply.github.com>
2023-09-20 17:42:20 +0200

    Update models.py

--- doctolibbydjango/authentification/models.py
+++ doctolibbydjango/authentification/models.py
@@ -2,10 +2,34 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Utilisateur(AbstractUser):
-    pass
 
-# Trop long, la flemme
-#class Connexion(models.Model):
-#    username = models.CharField(max_length=50)
-#    motDePasse = models.CharField(max_length=50)
+    lesRoles = (
+        ('patient', 'patient'),
+        ("medecin", 'medecin'),
+        ("responsable", "responsable"),
+    )
 
+    role = models.CharField(max_length=30, 
+                            choices=lesRoles, 
+                            verbose_name='Rôle', null=True)
+
+
+"""
+#Trop long, la flemme
+class Connexion(models.Model):
+    username = models.CharField(max_length=50)
+
+    motDePasse = models.CharField(max_length=50)
+"""
+
+class medecinPatient(models.Model):
+    idPatient = models.ForeignKey(Utilisateur, 
+                                  null=True, 
+                                  on_delete=models.CASCADE,
+                                   related_name='patientMedecin', 
+                                   unique=True)
+    idMedecin = models.ForeignKey(Utilisateur, 
+                                  null=True, 
+                                  on_delete=models.CASCADE,
+                                   related_name='medecinPatient')
+    
