88c974f6bbe996746aae3878d793c8ea1005b888
Patrick-Wampe <47600274+Patrick-Wampe@users.noreply.github.com>
2023-10-19 09:16:58 +0200

    Update admin.py

--- doctolibbydjango/application/admin.py
+++ doctolibbydjango/application/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from application.models import FormulaireSante
 
-# Register your models here.
+
+class colonnes(admin.ModelAdmin):
+    #list_display = ("id", "username", "role", "email","is_superuser",)  #[field.name for field in Utilisateur._meta.get_fields()]
+    list_display = [field.name for field in FormulaireSante._meta.get_fields()]
+
+admin.site.register(FormulaireSante, colonnes)
